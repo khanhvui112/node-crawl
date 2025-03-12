099a8447f265654d4db106e8ba609c2e36cc2c0a
Khanh Vui <vuisk@chipo.asia>
2025-03-12 14:38:35 +0700

    u

---
+++ config/config.js
@@ -0,0 +1,6 @@
+exports.config = {
+  "host": "zproxy.lum-superproxy.io",
+  "port": 22225,
+  "username": "brd-customer-hl_ba3dd393-zone-nam_proxy_taob",
+  "password": "pjxzc52s5fg1"
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import * as superBuy from "./superbuy.js";
+const app = express();
+const port = 3000;
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+const apiRouter = express.Router();
+apiRouter.post("/superbuy", async (req, res) => {
+    const body = req.body;
+    if (!body.originUrl) {
+        return res.status(200).json({
+            success: false,
+            message: "Dữ liệu không hợp lệ. Vui lòng kiểm tra lại"
+        });
+    }
+    try {
+        let result = await superBuy.commonCallPuppeteer(req.headers, body);
+        // console.log(result)
+        res.json(result.data);
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
+app.use("/api", apiRouter);
+app.listen(port, () => {
+    console.log(`API đang chạy tại http://localhost:${port}/api`);
+});
+app.use((req, res, next) => {
+    console.log("Incoming request:");
+    console.log("Method:", req.method);
+    console.log("URL:", req.originalUrl);
+    console.log("Headers:", req.headers);
+    console.log("Body:", req.body);
+    next();
+});
