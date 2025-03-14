d5f03d7b1cc2d0065ed8791a63a46922c041eb64
Khanh Vui <vuisk@chipo.asia>
2025-03-14 13:41:39 +0700

    u

--- superbuy.js
+++ superbuy.js
@@ -42,8 +42,8 @@ async function commonCallPuppeteer(headers, body) {
     const browser = await puppeteer.launch(puppeteerOptions);
     const page = await browser.newPage();
     await page.deleteCookie(...(await page.cookies()));
-    await page.evaluate(() => localStorage.clear());
-    await page.evaluate(() => sessionStorage.clear());
+    // await page.evaluate(() => localStorage.clear());
+    // await page.evaluate(() => sessionStorage.clear());
     if (useProxy) {
         // Xác thực Proxy
         await page.authenticate({
