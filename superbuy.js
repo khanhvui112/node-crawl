2fa65bc15b60456fb2db15b7f9ccaec9f9d09dec
Khanh Vui <vuisk@chipo.asia>
2025-03-26 17:15:51 +0700

    u

--- superbuy.js
+++ superbuy.js
@@ -51,11 +51,23 @@ async function setCookiesFromHeader(page, cookieHeader) {
 
     }
 }
+async function getIP(page) {
+    try {
+        await page.goto("https://checkip.amazonaws.com/", { waitUntil: 'networkidle2' });
 
+        const ipData = await page.evaluate(() => document.body.textContent.trim());
+
+        console.log('Your IP:', ipData);
+        return ipData;
+    } catch (error) {
+        console.error("Lỗi lấy IP:", error);
+        return null;
+    }
+}
 async function commonCallPuppeteer(headers, body) {
     puppeteer.use(StealthPlugin());
-
-    const browser = await puppeteer.launch({
+    let url = '';
+    let puppeteerOptions = {
         headless: true,
         args: [
             "--no-sandbox",
@@ -64,11 +76,34 @@ async function commonCallPuppeteer(headers, body) {
             "--disable-features=IsolateOrigins,site-per-process",
             "--disable-blink-features=AutomationControlled"
         ]
-    });
+    };
+    let useProxy = false;
+    let proxyUsername = '', proxyPassword = '';
+    if (body.originUrl) {
+        url = body.originUrl;
+        delete body.originUrl;
+    }
+    if (body.proxyUrl) {
+        const proxy = 'http://' + body.proxyUrl.toString().split("@")[1];
+        const auth = body.proxyUrl.toString().split("@")[0].replace(/^https?:\/\//, '');
+        proxyUsername = auth.split(":")[0]
+        proxyPassword = auth.split(":")[1]
+        puppeteerOptions.args = [...puppeteerOptions.args, `--proxy-server=${proxy}`]
+        delete body.proxyUrl;
+        useProxy = true;
+    }
+    const browser = await puppeteer.launch(puppeteerOptions);
 
     const page = (await browser.pages())[0]; // Lấy tab đầu tiên
+    if (useProxy) {
+        // Xác thực Proxy
+        await page.authenticate({
+            username: proxyUsername,
+            password: proxyPassword
+        });
+    }
     await page.setUserAgent(randomUserAgent());
-
+    const originalIP = await getIP(page);
     //Chặn tài nguyên không cần thiết
     await page.setRequestInterception(true);
     page.on("request", (req) => {
@@ -94,20 +129,22 @@ async function commonCallPuppeteer(headers, body) {
         headers: {...headers, "sec-fetch-site": "same-site"},
         body: JSON.stringify(body)
     };
-
+    //const originalIP = await getIP(page);
     try {
         // Gửi request trực tiếp từ Puppeteer bằng page.evaluateHandle()
-        const responseHandle = await page.evaluateHandle(async (fetchUrl, fetchOptions) => {
+        const responseHandle = await page.evaluateHandle(async (url, fetchOptions) => {
             fetchOptions.credentials = "include"; //Giữ cookie
-            const res = await fetch(fetchUrl, fetchOptions);
-            return res.json();
-        }, body.originUrl, requestOptions);
+            const res = await fetch(url, fetchOptions);
+            //const res = await fetch('https://api.myip.com/', {});
+            return await res.json();
+        }, url, requestOptions);
 
         const response = await responseHandle.jsonValue(); //Lấy dữ liệu từ handle
         await responseHandle.dispose(); //Giải phóng tài nguyên
         await browser.close();
         return {status: 200, data: response, message: "Thành công", success: true};
     } catch (error) {
+        console.log(error)
         await browser.close();
         return {status: 500, data: null, message: "Thất bại", success: false, error: error.message};
     }
